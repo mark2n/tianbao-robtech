bddc9c36b9bb10154778d14b1705a1b697ebc6f3
Tianbao <tianbao.zhang@yahoo.com>
2017-03-24 19:03:26 +0200

    week 28, teleop r2d2

---
+++ build/my_r2d2/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/tudeng/tianbao-robtech/src/my_r2d2
+# Build directory: /home/tudeng/tianbao-robtech/build/my_r2d2
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
